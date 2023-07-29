803af1c07587188d6543f28439b93cf1383a7080
wung brandon <wungbrandon27@gmail.com>
2023-07-29 06:49:16 +0300

    initial commit

---
+++ day1/bandname.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator ")
+
+city = input("What's the name of the city you grew up in?:\n")
+print(city)
+
+pet_name = input("What's Your pet name?:\n")
+print(pet_name)
+
+print("Your Band Name could be", city + " " + pet_name)
---
+++ day1/print.py
@@ -0,0 +1,22 @@
+#printing something in the console
+
+print("Hello world!")
+
+print('Day 1 - Python print function')
+print("The python function is declared like this:")
+print("print('What to print')")
+
+#string manipulation
+print("hello\nworld!") #next line
+print("wung" + " " + "Brandon")
+
+#python input function
+print("Hello" + " " + input("What is your name?"))
+
+#variables
+name = input("What is your name?: ")
+print("The length of your name is ",len(name))  #using the len function to get the length of the variable
+print(name)
+
+fname = 'wung' #the variable here is fname
+print(fname)
